a925f939e6374f7119bd281999c95d502e1eb170
Sorin Chiruta <sorinchiruta@mgil.com>
2025-10-15 00:59:52 +0300

    Remove sensitive information output from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,7 +57,7 @@ function generate_litespeed_password() {
   if [ -n "${ADMIN_PASSWORD}" ]; then
     ENCRYPT_PASSWORD="$(/usr/local/lsws/admin/fcgi-bin/admin_php -q '/usr/local/lsws/admin/misc/htpasswd.php' "${ADMIN_PASSWORD}")"
     echo "admin:${ENCRYPT_PASSWORD}" >'/usr/local/lsws/admin/conf/htpasswd'
-    echo "WebAdmin user/password is admin/${ADMIN_PASSWORD}" >'/usr/local/lsws/adminpasswd'
+
   fi
 }
 
@@ -244,8 +244,7 @@ service memcached start
 # welcome to dockerpress
 sysvbanner dockerpress
 
-# Read the credentials
-cat '/usr/local/lsws/adminpasswd'
+
 
 # Tail the logs to stdout
 tail -f \
