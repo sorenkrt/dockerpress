18a30e215e8a1882049302687430f40ea92b52c5
Sorin Chiruta <sorinchiruta@mgil.com>
2025-10-23 17:55:14 +0300

    Fix indentation in WebAdmin password output in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,7 +57,7 @@ function generate_litespeed_password() {
   if [ -n "${ADMIN_PASSWORD}" ]; then
     ENCRYPT_PASSWORD="$(/usr/local/lsws/admin/fcgi-bin/admin_php -q '/usr/local/lsws/admin/misc/htpasswd.php' "${ADMIN_PASSWORD}")"
     echo "admin:${ENCRYPT_PASSWORD}" >'/usr/local/lsws/admin/conf/htpasswd'
-        echo "WebAdmin user/password is admin/${ADMIN_PASSWORD}" >'/usr/local/lsws/adminpasswd'
+    echo "WebAdmin user/password is admin/${ADMIN_PASSWORD}" >'/usr/local/lsws/adminpasswd'
   fi
 }
 
